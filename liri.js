c5278195d82d3d42770e4d24a22dc57babf4fc13
Mobious <sammiel.gross@gmail.com>
2018-09-10 13:14:50 -0500

    start

---
+++ liri.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const fs = require('fs');
+const spotify = require('node-spotify-api');
+const request = require('request');
+const command = process.argv[2];
+const query = process.argv[3];
+
+const keys = require("./keys");
+console.log(process.env.SPOTIFY_ID) 
+console.log(process.env.SPOTIFY_SECRET)
+
+debugger;
+
+// const spotify = new Spotify(keys.spotify);
+
+
+// const spotifyThisSong = function (trackQuery) {
+//     const spotify = require("spotify");
+
+//     if (trackQuery === undefined) {
+//         trackQuery = "slappin da base man";
+//     }
+
+//     spotify.search({
+//             type: 'track',
+//             query: trackQuery
+//         },
+//         (error, data) => {
+//             if (error) throw error;
+//             else {
+//                 for (i = 0; i < data.tracks.items[0].artists.length; i++) {
+//                     if (i === 0) {
+//                         console.log('Artist:   ' + data.tracks.items[0].artists[i].name);
+//                     } else {
+//                         console.log('   ' + data.tracks.items[0].artists[i].name);
+
+//                     }
+//                 }
+//             }
+//             console.log("Song:         " + data.tracks.items[0].name);
+//             console.log("Preview Link: " + data.tracks.items[0].preview_url);
+//             console.log("Album:        " + data.tracks.items[0].album.name);
+//         }
+//     )
+
+// }
+// debugger;
