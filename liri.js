2cf3795bb0cdffd927a9f7366e5018d61311c9fa
Mobious <sammiel.gross@gmail.com>
2018-09-11 18:17:58 -0500

    env feex

--- liri.js
+++ liri.js
@@ -12,37 +12,37 @@ console.log(process.env.SPOTIFY_SECRET)
 
 debugger;
 
-// const spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 
 
-// const spotifyThisSong = function (trackQuery) {
-//     const spotify = require("spotify");
+const spotifyThisSong = function (trackQuery) {
+    // const spotify = require("spotify");
 
-//     if (trackQuery === undefined) {
-//         trackQuery = "slappin da base man";
-//     }
+    if (trackQuery === undefined) {
+        trackQuery = "slappin da base man";
+    }
 
-//     spotify.search({
-//             type: 'track',
-//             query: trackQuery
-//         },
-//         (error, data) => {
-//             if (error) throw error;
-//             else {
-//                 for (i = 0; i < data.tracks.items[0].artists.length; i++) {
-//                     if (i === 0) {
-//                         console.log('Artist:   ' + data.tracks.items[0].artists[i].name);
-//                     } else {
-//                         console.log('   ' + data.tracks.items[0].artists[i].name);
+    spotify.search({
+            type: 'track',
+            query: trackQuery
+        },
+        (error, data) => {
+            if (error) throw error;
+            else {
+                for (i = 0; i < data.tracks.items[0].artists.length; i++) {
+                    if (i === 0) {
+                        console.log('Artist:   ' + data.tracks.items[0].artists[i].name);
+                    } else {
+                        console.log('   ' + data.tracks.items[0].artists[i].name);
 
-//                     }
-//                 }
-//             }
-//             console.log("Song:         " + data.tracks.items[0].name);
-//             console.log("Preview Link: " + data.tracks.items[0].preview_url);
-//             console.log("Album:        " + data.tracks.items[0].album.name);
-//         }
-//     )
+                    }
+                }
+            }
+            console.log("Song:         " + data.tracks.items[0].name);
+            console.log("Preview Link: " + data.tracks.items[0].preview_url);
+            console.log("Album:        " + data.tracks.items[0].album.name);
+        }
+    )
 
-// }
-// debugger;
+}
+debugger;
