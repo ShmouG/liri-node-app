f917007fe2e7153e99e356f0043e56fa0d5954f5
Mobious <sammiel.gross@gmail.com>
2018-09-13 23:21:14 -0500

    added omdb

--- liri.js
+++ liri.js
@@ -8,15 +8,15 @@ const query = process.argv[3];
 const space = '\n';
 const dataKeys = require("./keys");
 
-let writeToLog = function(data) {
+let writeToLog = function (data) {
   fs.appendFile("random.text", '\r\n\r\n');
 
-  fs.appendFile("random.text", JSON.stringify(data), function(err) {
-      if (err) {
-          return console.log(err);
-      }
+  fs.appendFile("random.text", JSON.stringify(data), function (err) {
+    if (err) {
+      return console.log(err);
+    }
 
-      console.log("log.txt was updated!");
+    console.log("log.txt was updated!");
   });
 }
 
@@ -24,82 +24,82 @@ function getMeSpotify(songName) {
   let spotify = new Spotify(dataKeys.spotify);
 
   if (!songName) {
-      songName = "What's my age again";
+    songName = "White and nerdy";
   }
 
-  spotify.search({ type: 'track', query: songName }, function(err, data) {
-      if (err) {
-          console.log('Error occurred: ' + err);
-          return;
-      } else {
-          output = space + "================= LIRI FOUND THIS FOR YOU...==================" +
-              space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
-              space + "Album Name: " + data.tracks.items[0].album.name +
-              space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
-              space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
-          console.log(output);
+  spotify.search({
+    type: 'track',
+    query: songName
+  }, function (err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    } else {
+      output = space + "================= LIRI FOUND THIS FOR YOU...==================" +
+        space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
+        space + "Album Name: " + data.tracks.items[0].album.name +
+        space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
+        space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
+      console.log(output);
 
-          fs.appendFile("log.txt", output, function(err) {
-              if (err) throw err;
-              console.log('Saved!');
-          });
-      };
+      fs.appendFile("log.txt", output, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    };
   });
 }
 
+let getMeMovie = function (movieName) {
+  if (!movieName) {
+    movieName = "Tetsuo: The Iron man";
+  }
+
+  let queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  request(queryURL, function (err, res, body) {
+    if (err) throw err;
+    else {
+      let jsonData = JSON.parse(body);
+      console.log(jsonData);
+
+      output = space + "================= LIRI FOUND THIS FOR YOU...==================" +
+        space + 'Title: ' + jsonData.Title +
+        space + 'Year: ' + jsonData.Year +
+        space + 'Rated: ' + jsonData.Rated +
+        space + 'IMDB Rating: ' + jsonData.imdbRating +
+        space + 'Country: ' + jsonData.Country +
+        space + 'Language: ' + jsonData.Language +
+        space + 'Plot: ' + jsonData.Plot +
+        space + 'Actors: ' + jsonData.Actors +
+        space + 'Tomato Rating: ' + jsonData.Ratings[1].Value +
+        space + 'IMDb Rating: ' + jsonData.imdbRating + "\n";
+
+      console.log(output);
+
+      fs.appendFile("log.txt", output, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+        space
+      });
+    }
+  });
+
+}
+
 const pick = function (caseData, functionData) {
-  switch (caseData){
+  switch (caseData) {
     case 'spotify-this-song':
-    getMeSpotify(functionData)
+      getMeSpotify(functionData)
+      break;
+    case 'movie-this':
+    getMeMovie(functionData)
     break;
   }
 }
 //run this on load of js file
-let runThis = function(argOne, argTwo) {
+let runThis = function (argOne, argTwo) {
   pick(argOne, argTwo);
 };
 
 runThis(process.argv[2], process.argv[3]);
-
-debugger;
-
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
-
-// const spotifyThisSong = function (trackQuery) {
-//     // const spotify = require("spotify");
-
-//     if (trackQuery === undefined) {
-//         trackQuery = "slappin da base man";
-//     }
-
-//     spotify.search({
-//             type: 'track',
-//             query: trackQuery
-//         },
-//         function (error, data) {
-//             if (error) throw error;
-//             else {
-//                 for (i = 0; i < data.tracks.items[0].artists.length; i++) {
-//                     if (i === 0) {
-//                         console.log('Artist:   ' + data.tracks.items[0].artists[i].name);
-//                     } else {
-//                         console.log('   ' + data.tracks.items[0].artists[i].name);
-
-//                     }
-//                 }
-//             }
-//             console.log("Song:         " + data.tracks.items[0].name);
-//             console.log("Preview Link: " + data.tracks.items[0].preview_url);
-//             console.log("Album:        " + data.tracks.items[0].album.name);
-//         }
-//     )
-
-// }
-// debugger;
